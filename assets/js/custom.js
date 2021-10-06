4f18f19215bb1201cfb22238849201a527005dfa
toto4dlottery <91359888+toto4dlottery@users.noreply.github.com>
2021-10-06 19:55:00 +0600

    toto.net
    toto.net

---
+++ assets/js/custom.js
@@ -0,0 +1,12 @@
+var preloader = document.getElementById('loading');
+function myfunction(){
+    preloader.style.display ='none';
+}
+ $(document).ready(function() {
+          
+            $(".m-toggle").click(function() {
+                $(".table-striped").toggle(1000);
+            })
+            
+        })
+
